dbe4a5af59a55e7316e4b3f0ad08dae9860eee37
Klaus Steinmeier <klaus.steinmeier@sap.com>
2025-06-14 09:45:43 -0300

    refactor profanity check

--- api/chirp_validation_handler.go
+++ api/chirp_validation_handler.go
@@ -3,9 +3,16 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/klausks/bootdev-chirpy/internal/webutils"
 )
+
+var profanities = map[string]bool{
+	"kerfuffle": true,
+	"sharbert":  true,
+	"fornax":    true,
+}
 
 func HandleValidateChirpReq(rw http.ResponseWriter, req *http.Request) {
 	type body struct {
@@ -13,17 +20,29 @@ func HandleValidateChirpReq(rw http.ResponseWriter, req *http.Request) {
 	}
 
 	type validResponse struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	reqBody := body{}
 	if err := json.NewDecoder(req.Body).Decode(&reqBody); err != nil {
 		webutils.RespondWithError(rw, 500, "Something went wrong")
+		return
 	}
 
 	if len(reqBody.Body) > 140 {
 		webutils.RespondWithError(rw, 400, "Chirp is too long")
-	} else {
-		webutils.RespondWithJSON(rw, 200, validResponse{Valid: true})
+		return
 	}
+	webutils.RespondWithJSON(rw, 200, validResponse{CleanedBody: removeProfanity(reqBody.Body)})
+}
+
+func removeProfanity(text string) string {
+	strings.Split(text, "")
+	words := strings.Split(text, " ")
+	for i, word := range words {
+		if _, exists := profanities[strings.ToLower(word)]; exists {
+			words[i] = "****"
+		}
+	}
+	return strings.Join(words, " ")
 }
